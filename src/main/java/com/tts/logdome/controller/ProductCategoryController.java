b3b592691bd49fc68b3e0291ce819bf7aa305398
zhoupeng521 <m13266804203@163.com>
2019-05-27 18:06:42 +0800

    添加controller

---
+++ src/main/java/com/tts/logdome/controller/ProductCategoryController.java
@@ -0,0 +1,15 @@
+package com.tts.logdome.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *@描述 商品类目Controller
+ *@创建人  zp
+ *@创建时间  2019/5/27
+ */
+@Controller
+public class ProductCategoryController {
+
+
+
+}
