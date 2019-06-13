3dc4e6068d98121594db5cf83fa2650b292c8a53
zhoupeng521 <m15669082438@163.com>
2019-06-14 01:19:54 +0800

    支付宝

--- src/main/java/com/tts/logdome/common/utils/AlipayUtils.java
+++ src/main/java/com/tts/logdome/common/utils/AlipayUtils.java
@@ -4,18 +4,20 @@ import com.alipay.api.AlipayClient;
 import com.alipay.api.DefaultAlipayClient;
 import com.tts.logdome.common.config.AlipayConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * @创建人 zp
  * @创建时间 2019/6/13
  * @描述 支付宝--工具类
  */
+@Component
 public class AlipayUtils {
 
     @Autowired
-    private static AlipayConfig alipayConfig;
+    private AlipayConfig alipayConfig;
 
-    public static AlipayClient getAlipayClient(){
+    public AlipayClient getAlipayClient(){
         AlipayClient alipayClient = new DefaultAlipayClient(alipayConfig.getUrl(), alipayConfig.getAppId(), alipayConfig.getAppPrivateKey(),
                 alipayConfig.getFormat(), alipayConfig.getCharset(), alipayConfig.getAlipayPublicKey(), alipayConfig.getSignType());
         return  alipayClient;
