d434cff2f4746adf416bcef9d7c0f864c8306fd7
zhoupeng521 <m13266804203@163.com>
2019-07-25 18:34:20 +0800

    【每天一学】直接插入排序

---
+++ src/main/java/com/tts/logdome/common/queue/DirectInsertSort.java
@@ -0,0 +1,23 @@
+package com.tts.logdome.common.queue;
+
+/**
+ * @创建人 zp
+ * @创建时间 2019/7/25
+ * @描述 直接插入排序算法
+ */
+public class DirectInsertSort {
+
+    public static void main(String[] args) {
+
+        int[] number = new int[]{1,23,2,45,11,55,12,24,13,15,14};
+        int temp;
+        int j;
+        for (int i = 0; i < number.length ; i ++){
+            //插入
+            temp = number[i];
+
+        }
+
+    }
+
+}
