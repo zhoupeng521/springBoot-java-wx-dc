e8c359761ed8a12fb925a2b59d8d9f931904a5f3
zhoupeng521 <m13266804203@163.com>
2019-08-08 18:37:16 +0800

    【每天一学】学习知识并发编程AQS

---
+++ src/main/java/com/tts/logdome/common/thread/FinalTest.java
@@ -0,0 +1,27 @@
+package com.tts.logdome.common.thread;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @创建人 zp
+ * @创建时间 2019/8/8
+ * @描述 final的使用
+ */
+public class FinalTest {
+
+    static String as = "ewqweqeq";
+
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+        lock.lock();
+        change();
+        System.out.println(as);
+        lock.unlock();
+    }
+
+    public static void change(){
+        as = "dasdasd";
+    }
+
+}
---
+++ src/main/java/com/tts/logdome/common/thread/LockTest.java
@@ -0,0 +1,108 @@
+package com.tts.logdome.common.thread;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @创建人 zp
+ * @创建时间 2019/8/8
+ * @描述 AQS初步理解
+ */
+public class LockTest implements Lock,java.io.Serializable{
+
+    // Our internal helper class
+    // 继承AQS的静态内存类
+    // 重写方法
+    static class Sync extends AbstractQueuedSynchronizer{
+
+        protected Sync() {
+            super();
+        }
+
+        @Override
+        protected boolean tryAcquire(int acquires) {
+            assert acquires == 1; // Otherwise unused
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        protected boolean tryRelease(int releases) {
+            assert releases == 1; // Otherwise unused
+            if (getState() == 0) throw new IllegalMonitorStateException();
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        Condition newCondition(){
+            return new ConditionObject();
+        }
+
+        // Deserializes properly
+        private void readObject(ObjectInputStream s)
+                throws IOException, ClassNotFoundException {
+            s.defaultReadObject();
+            setState(0); // reset to unlocked state
+        }
+
+        @Override
+        protected int tryAcquireShared(int arg) {
+            return super.tryAcquireShared(arg);
+        }
+
+        @Override
+        protected boolean tryReleaseShared(int arg) {
+            return super.tryReleaseShared(arg);
+        }
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString();
+        }
+    }
+
+    private static final Sync sync = new Sync();
+    //使用同步器的模板方法实现自己的同步语义
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireSharedNanos(1,unit.toNanos(time));
+    }
+
+    @Override
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+}
